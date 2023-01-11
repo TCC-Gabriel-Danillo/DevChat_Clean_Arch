8164c4150c72452825388a34d410e020048f49dd
GabrielDvPereira <gdavi.gd@gmail.com>
2023-01-11 20:52:49 -0300

    feat(infra/helpers): add firebase helpres

---
+++ src/infra/helpers/parseFirebaseCollection.ts
@@ -0,0 +1,14 @@
+import { 
+    Firestore, 
+    collection, 
+    CollectionReference,
+    DocumentData,
+} from 'firebase/firestore';
+
+export const parseCollection = (collections: string[], firestore: Firestore): CollectionReference<DocumentData> => {
+    if(collection.length > 1){
+        return collection.apply(null, [firestore, ...collections])
+    }
+
+    return collection(firestore, collections[0])
+}
