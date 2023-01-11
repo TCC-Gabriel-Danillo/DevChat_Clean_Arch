d59bf8a572d3e5d7c81e6e66235373060d0fcd67
GabrielDvPereira <gdavi.gd@gmail.com>
2023-01-11 20:58:59 -0300

    feat(presentation/constants): add screens constants

---
+++ src/presentation/constants/screens.ts
@@ -0,0 +1,11 @@
+export const APP_SCREENS = {
+    Auth: {
+        AushScreen: "AUTH_SCREEN"
+    }, 
+    Main: {
+        HomeScreen: "HOME_SCREEN",
+        MessageScreen: "MESSAGE_SCREEN", 
+        TechScreen: "TECH_SCREEN", 
+        UsersScreen: "USERS_SCREEN"
+    }
+}
