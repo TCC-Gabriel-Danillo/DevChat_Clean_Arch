27af2567e4b0e5149535d70101568b297cc19d62
GabrielDvPereira <gdavi.gd@gmail.com>
2023-01-12 19:03:37 -0300

    refactor(infra/cache): rename localstorage class

--- src/infra/cache/localStorage.ts
+++ src/infra/cache/localStorage.ts
@@ -1,22 +1,22 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { default as RnAsyncStorage } from '@react-native-async-storage/async-storage'
 import { LocalStorage } from '_/data/protocols/cache/localStorage'
 
-export class LocalStorageImp implements LocalStorage {
+export class AsyncStorage implements LocalStorage {
   async getItem<T>(key: string): Promise<T | undefined> {
-    const data = await AsyncStorage.getItem(key)
+    const data = await RnAsyncStorage.getItem(key)
     if (!data) return
     const dataObj = JSON.parse(data)
     return dataObj
   }
   async setItem(key: string, data: any): Promise<void> {
     if (typeof data === 'string') {
-      await AsyncStorage.setItem(key, data)
+      await RnAsyncStorage.setItem(key, data)
       return
     }
     const dataStr = JSON.stringify(data)
-    await AsyncStorage.setItem(key, dataStr)
+    await RnAsyncStorage.setItem(key, dataStr)
   }
   async removeItem(key: string): Promise<void> {
-    await AsyncStorage.removeItem(key)
+    await RnAsyncStorage.removeItem(key)
   }
 }
