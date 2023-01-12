41204fabd6097e47004ad410619ed48699db02e3
GabrielDvPereira <gdavi.gd@gmail.com>
2023-01-12 19:09:52 -0300

    feat(main/factories): add barrel file

---
+++ src/main/factories/index.ts
@@ -0,0 +1,4 @@
+export * from "./usecases/authServiceFactory"
+export * from "./usecases/conversationServiceFactory"
+export * from "./usecases/messageServiceFactory"
+export * from "./usecases/userServiceFactory"
