7255433caec4387d9573947d22cbdfe6b63b3218
GabrielDvPereira <gdavi.gd@gmail.com>
2023-01-15 16:04:44 -0300

    test(mocks/stub): add barrel file

---
+++ __tests__/mocks/stubs/index.ts
@@ -0,0 +1,8 @@
+
+export * from "./conversationServiceStub"
+export * from "./userServiceStub"
+export * from "./authPromptServiceStub"
+export * from "./authServiceStub"
+export * from "./cacheStub"
+export * from "./databaseRepositoryStub"
+export * from "./realtimeDatabaseRepositorySub"
