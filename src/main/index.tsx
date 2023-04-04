1b771a0f81d8b91e13d920f7080e611c67297796
GabrielDvPereira <gdavi.gd@gmail.com>
2023-04-03 23:57:56 -0300

    chore: config react query

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,14 @@
 import "./config/firebase"
 
 import { View } from "react-native"
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import { useCustomFonts } from "_/presentation/hooks"
 import { AlertProviderComposer, AuthProviderComposer } from "./composes"
 import { Navigation } from "./navigation"
+
+const queryClient = new QueryClient()
 
 export const Main = () => {
     const [isLoaded] = useCustomFonts()
@@ -11,10 +16,12 @@ export const Main = () => {
     if(!isLoaded) return <View />
 
     return (
-        <AlertProviderComposer>
-            <AuthProviderComposer>
-                <Navigation />
-            </AuthProviderComposer>
-        </AlertProviderComposer>
+        <QueryClientProvider client={queryClient}>
+            <AlertProviderComposer>
+                <AuthProviderComposer>
+                    <Navigation />
+                </AuthProviderComposer>
+            </AlertProviderComposer>
+        </QueryClientProvider>
     )
 }
